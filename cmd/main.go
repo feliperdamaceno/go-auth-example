d208b82fb830f097060e55c88c17689816717217
feliperdamaceno <feliperdamaceno@sent.com>
2025-04-11 01:32:03 +0100

    feat: finish routes

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth/v5"
 	"github.com/go-chi/render"
 	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
@@ -18,13 +21,16 @@ type User struct {
 	Password string `json:"password"`
 }
 
-type SimplePayload struct {
+type UsersPayload struct {
+	Users []User `json:"users"`
+}
+
+type MessagePayload struct {
 	Message string `json:"message"`
 }
 
-type Database = map[string]User
-
-var database = make(Database)
+var database = make(map[string]User)
+var tokenAuth *jwtauth.JWTAuth
 
 func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), 4)
@@ -36,9 +42,17 @@ func HashPassword(password string) (string, error) {
 	return string(hash), nil
 }
 
-func ValidatePassword(hashed string, password string) bool {
+func IsValidatePassword(hashed string, password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashed), []byte(password))
 	return err == nil
+}
+
+func GenerateAuthToken(tokenAuth *jwtauth.JWTAuth, token map[string]interface{}) (string, error) {
+	_, tokenString, err := tokenAuth.Encode(token)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
 }
 
 func main() {
@@ -47,14 +61,16 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	// var JWT_SECRET = os.Getenv("JWT_SECRET")
+	var ENV = os.Getenv("ENV")
+	var JWT_SECRET = os.Getenv("JWT_SECRET")
+	tokenAuth = jwtauth.New("HS256", []byte(JWT_SECRET), nil)
 
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	router.Use(render.SetContentType(render.ContentTypeJSON))
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		payload := SimplePayload{Message: "Server Working!"}
+		payload := MessagePayload{Message: "Server Working!"}
 		render.JSON(w, r, payload)
 	})
 
@@ -64,15 +80,15 @@ func main() {
 		err := json.NewDecoder(r.Body).Decode(&user)
 
 		if err != nil || user.Email == "" || user.Password == "" {
-			w.WriteHeader(400)
-			payload := SimplePayload{Message: "Please provide valid credentials"}
+			w.WriteHeader(http.StatusBadRequest)
+			payload := MessagePayload{Message: "Please provide valid credentials"}
 			render.JSON(w, r, payload)
 			return
 		}
 
 		if database[user.Email].Email != "" {
-			w.WriteHeader(400)
-			payload := SimplePayload{
+			w.WriteHeader(http.StatusBadRequest)
+			payload := MessagePayload{
 				Message: fmt.Sprintf("User with email <%s> already exist", user.Email),
 			}
 			render.JSON(w, r, payload)
@@ -81,8 +97,8 @@ func main() {
 
 		hashedPassword, err := HashPassword(user.Password)
 		if err != nil {
-			w.WriteHeader(500)
-			payload := SimplePayload{
+			w.WriteHeader(http.StatusInternalServerError)
+			payload := MessagePayload{
 				Message: "Failed to hash the password",
 			}
 			render.JSON(w, r, payload)
@@ -94,7 +110,68 @@ func main() {
 		render.JSON(w, r, user)
 	})
 
-	router.Post("/login", func(w http.ResponseWriter, r *http.Request) {})
+	router.Post("/login", func(w http.ResponseWriter, r *http.Request) {
+		credentials := User{}
+
+		err := json.NewDecoder(r.Body).Decode(&credentials)
+		if err != nil || credentials.Email == "" || credentials.Password == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			payload := MessagePayload{Message: "Please provide valid credentials"}
+			render.JSON(w, r, payload)
+			return
+		}
+
+		user := database[credentials.Email]
+		if user.Email == "" {
+			w.WriteHeader(http.StatusNotFound)
+			payload := MessagePayload{
+				Message: fmt.Sprintf("User with email <%s> has not been found", credentials.Email),
+			}
+			render.JSON(w, r, payload)
+			return
+		}
+
+		isValidPassword := IsValidatePassword(user.Password, credentials.Password)
+		if !isValidPassword {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Println(user.Password)
+			fmt.Println(credentials.Password)
+			payload := MessagePayload{Message: "Invalid password provided"}
+			render.JSON(w, r, payload)
+			return
+		}
+
+		jwt, err := GenerateAuthToken(tokenAuth, map[string]interface{}{
+			"email": user.Email,
+			"exp":   time.Now().Unix() + 60*15,
+		})
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			payload := MessagePayload{
+				Message: "Failed to generate auth token",
+			}
+			render.JSON(w, r, payload)
+			return
+		}
+
+		cookie := http.Cookie{
+			Name:     "jwt",
+			Value:    jwt,
+			MaxAge:   900,
+			HttpOnly: ENV == "production",
+			SameSite: http.SameSiteStrictMode,
+			Secure:   ENV == "production",
+		}
+
+		http.SetCookie(w, &cookie)
+
+		payload := MessagePayload{
+			Message: fmt.Sprintf("User <%s> logged in successfully", user.Email),
+		}
+
+		render.JSON(w, r, payload)
+	})
 
 	router.Get("/users", func(w http.ResponseWriter, r *http.Request) {
 		users := make([]User, 0)
@@ -103,16 +180,36 @@ func main() {
 			users = append(users, user)
 		}
 
-		payload := struct {
-			Users []User `json:"users"`
-		}{
-			Users: users,
-		}
-
+		payload := UsersPayload{Users: users}
 		render.JSON(w, r, payload)
 	})
 
-	router.Get("/users/{email}", func(w http.ResponseWriter, r *http.Request) {})
+	router.Get("/users/{email}", func(w http.ResponseWriter, r *http.Request) {
+		email := chi.URLParam(r, "email")
+		user := database[email]
+
+		if user.Email == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			payload := MessagePayload{
+				Message: fmt.Sprintf("User with email <%s> has not been found", email),
+			}
+			render.JSON(w, r, payload)
+			return
+		}
+
+		render.JSON(w, r, user)
+	})
+
+	router.Group(func(auth chi.Router) {
+		auth.Use(jwtauth.Verifier(tokenAuth))
+		auth.Use(jwtauth.Authenticator(tokenAuth))
+
+		auth.Get("/auth/protected", func(w http.ResponseWriter, r *http.Request) {
+			_, claims, _ := jwtauth.FromContext(r.Context())
+			payload := MessagePayload{Message: fmt.Sprintf("Welcome %s", claims["email"])}
+			render.JSON(w, r, payload)
+		})
+	})
 
 	http.ListenAndServe(":3000", router)
 }
